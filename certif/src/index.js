30d214d3e322dc93aeb0e5471d145814b18b683b
kumbissk <ramataka81@gmail.com>
2022-07-17 02:50:45 +0200

    configuration des routes avec react-routerDom

--- certif/src/index.js
+++ certif/src/index.js
@@ -1,18 +1,20 @@
+/* eslint-disable no-unused-vars */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
