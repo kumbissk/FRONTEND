f8b59151f3d0410bce1d04228db2c909fe8c19ef
kumbissk <ramataka81@gmail.com>
2022-07-17 02:03:02 +0200

    le Foother

--- certif/src/components/FooterSection.js
+++ certif/src/components/FooterSection.js
@@ -26,15 +26,71 @@ const FooterSection = () => {
             </article>
             </section>
         </TextContent>
-        <ContentSocial></ContentSocial>
-        <Note></Note>
+        <ContentSocial>
+            <div className='navigation'>
+                <h6>Navigation</h6>
+                <ul>
+                    <li>
+                        <a href='/'>Accueil</a>
+                    </li>
+                    <li>
+                        <a href='/Nos Services'>Nos Services</a>
+                    </li>
+                    <li>
+                        <a href='/A propos'>A propos</a>
+                    </li>
+                    <li>
+                        <a href='/Contact'>Contact</a>
+                    </li>
+                </ul>    
+            </div>
+            <div className='support'>
+                <h6>Support</h6>
+                <ul>
+                    <li>
+                        <a href='/'>@flashExpress@support.com</a>
+                    </li>
+                    <li>
+                        <a href='/'>Telephone: +(221)77 456 89 70</a>
+                    </li>
+                    <li>
+                        <a href='/'>Fixe: +(221)33 333 55 66</a>
+                    </li>
+                    <li>
+                        <a href='/'>Localisation: PG5J+V8J, Voie de degagement N, Dakar</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='contact'>
+                <h6>Retrouvez-nous sur</h6>
+                <p className='para1'>
+                    <img src='/images/Twitter.png' alt='Twitter'/>
+                    <p>@flashExpress</p>
+                </p>
+                <p className='para2'>
+                    <img src='/images/Facebook.png' alt='Facebook'/>
+                    <p>flashExpress_lofficiel</p>
+                </p>
+                <p className='para3'>
+                    <img src='/images/Instagram.png' alt='Instagram'/>
+                    <p>flashExpress_lofficiel</p>
+                </p>
+                <p className='para4'>
+                    <img src='/images/LinkedIn.png' alt='LinkedIn'/>
+                    <p>flashExpress_lofficiel</p>
+                </p>
+            </div>
+        </ContentSocial>
+        <Note>
+            <p>Copyrigth © 2022 - flashExpress. Tous droits réservés.</p>
+        </Note>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
 width: 1905px;
-height: 745px;
+height: 645px;
 // margin-left: 0px;
 // margin-top: 10px;
 background: #EFF1F5;`;
@@ -155,8 +211,140 @@ const TextContent = styled.div`
     }
 `;
 
-const ContentSocial = styled.div``;
+const ContentSocial = styled.div`
+    div.navigation {
+        width: 115px;
+        height: 150px;
+        margin-left: 350px;
+        margin-top: 130px;
+    }
+    
+    div.navigation h6 {
+        width: 109px;
+        height: 21px;
+        margin-left: 20px;
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+    }
+    
+    div.navigation ul {
+        width: 165px;
+        height: 16px;
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 16px;
+        color: #292D31;
+        margin-left: -12px;
+        list-style: none;
+    }
+    
+    div.navigation ul li {
+        padding: 3px;
+        margin-top: 6px;
+    }
+    
+    div.navigation ul li a {
+        text-decoration: none;
+        color: #292D31;
+    }
+    
+    div.support {
+        width: 380px;
+        height: 159px;
+        margin-left: 730px;
+        margin-top: -150px;
+    }
+    
+    div.support h6 {
+        width: 84px;
+        height: 21px;
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+    }
+    
+    div.support ul {
+        width: 464px;
+        height: 16px;
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 16px;
+        color: #292D31;
+        list-style: none;
+        margin-left: -32px;
+    }
+    
+    div.support ul li {
+        padding: 3px;
+        margin-top: 6px;
+    }
+    
+    div.support ul li a {
+        text-decoration: none;
+        color: #292D31;
+    }
 
-const Note = styled.div``;
+    div.contact {
+        width: 221px;
+        height: 162.43px;
+        margin-left: 1349px;
+        margin-top: -160px;
+    }
+
+    div.contact h6 {
+        width: 221px;
+        height: 20.74px;
+        margin-left: -14px;
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+        margin-top: -20px;
+    }
+
+    div.contact img {
+        width: 30.67px;
+        height: 27.65px;
+        margin-left: -24px;
+        margin-top: 20px;
+        cursor: pointer;
+    }
+
+    div.contact p {
+        width: 98px;
+        height: 46px;
+        margin-left: 18px;
+        margin-top: -25px;
+    }
+
+`;
+
+const Note = styled.div`
+    width: 561px;
+    height: 32px;
+    margin-left: 739px;
+    margin-top: 100px;
+    
+    p {
+        font-family: 'Epilogue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+    }`;
 
 export default FooterSection;
